24827983aa6364b415d8b9a1b520b0104e0a934f
harshdeepsingh8 <harshdeepsingh8@rrc.ca>
2026-02-24 12:53:22 -0600

    Fixed Java file for Super-Linter
    Refactor App class to prevent instantiation and add documentation.

--- App.java
+++ App.java
@@ -1,5 +1,18 @@
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello from Java");
+/**
+ * Simple App class
+ */
+public final class App {
+
+    private App() {
+        // Prevent instantiation
+    }
+
+    /**
+     * Main method
+     * @param args command line arguments
+     */
+    public static void main(final String[] args) {
+        final String name = "Harsh";
+        System.out.println("Hello " + name);
     }
 }
